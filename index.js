14667090f24f6478e90823d29371007d5dc60b9c
Kieren <kierenhuynh88@gmail.com>
2023-01-03 22:37:14 +1100

    fixed, working version

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-const Gameboard = (() => {
+const gameBoard = (() => {
     const boardContainer = document.querySelector('#board-container')
-    const cards = document.querySelectorAll('.card')
 
-    let board = 
-    [
-    '', 'gg', '', // board[0-8]
-    '', '', '',
-    '', '', ''
-    ]
+    let board = ['','','','','','','','','']
 
     const getField = (num) => board[num];
 
-    
     const displayBoard = () => {
         let num = 0;
         board.map(() => {
@@ -21,10 +14,11 @@ const Gameboard = (() => {
         })
     }
 
-    const setField = (num, player) => {
+    const setField = (num) => {
+        const marker = getTurn()
         const field = document.querySelector(`.card:nth-child(${num+1})`)
-        field.textContent = player.getSign()
-        board[num] = player.getSign()
+        field.textContent = marker
+        board[num] = marker
         displayBoard()
     }
 
@@ -48,120 +42,216 @@ const Gameboard = (() => {
         displayBoard()
     })()
 
+    const getTurn = () => {
+        if (board) {
+            let number = (board.reduce((aggregate, b) => aggregate+b))
+            if ((number.length % 2) == 0) {
+                return gameController.getPlayerOne().getSign()
+            } else {
+                return gameController.getPlayerTwo().getSign()
+            }
+        }
+    }
+
+    const findWinner = () => {
+        if (board) {
+            let number = (board.reduce((aggregate, b) => aggregate+b))
+            if ((number.length % 2) == 0) {
+                return gameController.getPlayerTwo().getSign()
+            } else {
+                return gameController.getPlayerOne().getSign()
+            }
+        }
+    }
 
 
-    return {updateBoard, board, setField, getField, cards}
+
+    return {updateBoard, board, setField, getField, getTurn, clearBoard, findWinner}
 })()
-
-// const counterObj = (() => {
-//     let count = 0
-//     const counter = () => {
-//         count++
-//         return count
-//     }
-
-//     return {counter}
-// })()
 
 
 //player factory
 
 
 const playerFactory = (marker) => {
-
-
-    const listener = (() => {
-        Gameboard.cards.forEach(card => card.addEventListener('click', (e) => {
-            console.log(e.target.id)
-            Gameboard.setField(3, playerOne)
-        }))
-    })()
-
-    const place = (e) => {
-
-    }
-
     const getSign = () => {
         return marker
     }
 
-    return {place, getSign}
+    return {getSign}
 }
 
-// define players 
-const playerOne = playerFactory('x')
-const playerTwo = playerFactory('o')
+const gameController = (() => {
+    const playerOne = playerFactory('X')
+    const playerTwo = playerFactory('O')
 
+    const getPlayerOne = () => playerOne
+    const getPlayerTwo = () => playerTwo
 
- Gameboard.setField(4, playerOne)
+    const _sleep = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
-// console.log(Gameboard.getField(4))
+    const checkForRows = (board) => {
+        for (i = 0; i < 9; i+=3) {
+            let row = []
+            for(n = 0; n < 3; n++) {
+                row.push(board[i+n])
+            }
+            console.log(row)
+            if(row.every(field => field == 'X') || row.every(field => field == 'O')) {
+                return true
+            }
+        }
+        return false
+    }
 
+    const checkForColumns = (board) => {
+        for (i = 0; i < 3; i++) {
+            let column = []
+            for(n = 0; n < 9; n+=3) {
+                column.push(board[i+n])
+            }
+            console.log(column)
+            if(column.every(field => field == 'X') || column.every(field => field == 'O')) {
+                return true
+            }
+        }
+        return false
+    }
 
-// const currentTile = (() => {
-//     const cards = document.querySelectorAll('.card')
+    const checkForDiagonals = (board) => {
+        let diagonal = []
+        diagonal.push(board[0], board[4], board[8])
+        if(diagonal.every(field => field == 'X') || diagonal.every(field => field == 'O')) {
+            return true
+        }
+        diagonal = []
+        diagonal.push(board[2], board[4], board[6])
+        if(diagonal.every(field => field == 'X') || diagonal.every(field => field == 'O')) {
+            return true
+        }
+        console.log(diagonal)
+        return false
 
-    // const currentID = () => {
-    //         cards.forEach(card => card.addEventListener('mouseover', (e) => {
-    //         console.log(e.target.id)
-    //         return e.target.id
+    }
+
+    const checkForWin = (board) => {
+        if (checkForColumns(board) || checkForRows(board) || checkForDiagonals(board)){
+            console.log('winner')
+            return true
+        } else {
+            console.log(board)
+            console.log('no win yet')
+            return false
+        }
+    }
+
+    const checkForDraw = (board) => {
+        if (board.every(field => field != '')) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    const playerStep = (num) => {
+        const field = gameBoard.getField(num);
+        if (field == '') {
+            gameBoard.setField(num);
+            if (checkForWin(gameBoard.board)) {
+                (async () => {
+                    await _sleep(500 + (Math.random() * 500));
+                    console.log('winnnnner')
+                    endGame(gameBoard.findWinner());
+                })();  
+            }
+            else if (checkForDraw(gameBoard.board)) {
+                (async () => {
+                    await _sleep(500 + (Math.random() * 500));
+                    console.log('drawwww')
+                    endGame("Draw");
+                })();  
+            }
+            else {
+                // //displayController.deactivate();
+                // (async () => {
+                //     await _sleep(250 + (Math.random() * 300));
+                //     aiStep();
+                //     if (!checkForWin(gameBoard)) {
+                //         displayController.activate();
+                //     }
+                // })();
+            }
+        }
+        else {
+            console.log('Already Filled')
+        }
+
+        const endGame = (sign) => {
+            if (sign == 'Draw') {
+                displayController.message.textContent = 'It was a draw!'
+            } else {
+                displayController.message.textContent = `The winner is ${sign}`
+            }
+            displayController.result.classList.add('active')
+            displayController.overlay.style.display = 'block'
+        }
+    }
+
+    return {
+        getPlayerOne,
+        getPlayerTwo,
+        playerStep,
+        checkForRows,
+        checkForColumns,
+        checkForDiagonals,
+        checkForWin,
+
+    }
+})()
+
+gameController.checkForColumns(gameBoard.board)
+
+gameController.checkForDiagonals(gameBoard.board)
+
+const displayController = (() => {
+    const result = document.querySelector('.result')
+    const message = document.querySelector('.message')
+    const overlay = document.querySelector('.overlay')
+    const closeBtn = document.querySelector('#closeBtn')
+
+    const closeBtnListener = (() => {
+        closeBtn.addEventListener('click', () => {
+            result.classList.remove('active')
+            overlay.style.display = 'none'
+            gameBoard.clearBoard()
+        })
+    })()
+
+    const cards = Array.from(document.querySelectorAll('.card'))
+
+    const init = (() => {
+        for (let i = 0; i < cards.length; i++) {
+            field = cards[i];
+            field.addEventListener('click', gameController.playerStep.bind(field, i));
+        }
+    })()
+
+    // const init = (() => {
+    //     cards.forEach(card => card.addEventListener('click', (e) => {
+    //         console.log(card)
+    //         gameController.playerStep(5)
     //     }))
-    // }
+    // })()
 
-//     const marker = () => {
-//             cards.forEach(card => card.addEventListener('mouseover', (e) => {
-//             console.log(e.target.textContent)
-//             console.log('heya')
-//             return e.target.textContent
-//         }))
-//     }
+        // restart.addEventListener('click', gameController.restart);
 
-//     return {currentID, marker}
-// })()
+        // x.addEventListener('click', _changePlayerSign.bind(this, 'X'));
 
+        // o.addEventListener('click', _changePlayerSign.bind(this, 'O'));
 
-
-// function getCoord(id) {
-//     row = idCoordinate[id].row
-//     column = idCoordinate[id].column
-//     return {row, column}
-// }
-
-
-
-
-// const runGame = () => {
-//     if ((counterObj.counter() % 2) == 0) {
-//         const cards = document.querySelectorAll('.card')
-
-//         cards.forEach((card) => card.addEventListener('click', (e) => {
-//             console.log('playeerone')
-//             playerOne.place(e)
-//             Gameboard.updateBoard()
-//         }))
-//     } else {
-//         const cards = document.querySelectorAll('.card')
-
-//         cards.forEach((card) => card.addEventListener('click', (e) => {
-//             console.log('PLAYERTWO')
-//             playerTwo.place(e)
-//             Gameboard.updateBoard()
-//         }))
-//     }
-// }
-
-
-// document.body.addEventListener('click', () => {
-//     runGame()
-//     currentTile.marker()
-// })
-
-
-
-
-
-
-
-
+    return {cards, result, message, overlay}
+})()
 
 
